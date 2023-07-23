e22c74c5d238a333e9ca3f50d0d1deff0bd9c2ec
Jeffery <140289025+NakedCabbage@users.noreply.github.com>
2023-07-22 19:00:53 -0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,10 @@
+
+wordlist = ["wordle","red","orange"]
+suspect = wordlist
+tuple = (1)
+print(len(suspect))
+for item in suspect:
+  
+#check every variable you can check and see which ones doesn't make sense
+#check where the wrong variable came from
+#check the type and length of the suspected variable
